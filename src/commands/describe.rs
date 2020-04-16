35e25927f67bbdc61b86634a31f83d4e35ee33c1
Paul Colomiets <paul@colomiets.name>
2020-04-16 17:15:06 +0300

    Implement highlight for the output of \describe
    Fixes #11

--- src/commands/describe.rs
+++ src/commands/describe.rs
@@ -4,9 +4,10 @@ use edgedb_protocol::value::Value;
 use crate::commands::Options;
 use crate::commands::helpers::quote_namespaced;
 use crate::client::Client;
+use crate::highlight;
 
 
-pub async fn describe<'x>(cli: &mut Client<'x>, _options: &Options,
+pub async fn describe<'x>(cli: &mut Client<'x>, options: &Options,
     name: &str, verbose: bool)
     -> Result<(), anyhow::Error>
 {
@@ -16,8 +17,12 @@ pub async fn describe<'x>(cli: &mut Client<'x>, _options: &Options,
             flag=if verbose { "VERBOSE" } else {""}),
         &Value::empty_tuple(),
     ).await?;
-    while let Some(name) = items.next().await.transpose()? {
-        println!("{}", name);
+    while let Some(text) = items.next().await.transpose()? {
+        if let Some(ref styler) = options.styler {
+            println!("{}", highlight::edgeql(&text, styler));
+        } else {
+            println!("{}", text);
+        }
     }
     Ok(())
 }
