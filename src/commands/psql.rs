8fbddc7a1e0b3a6508b9bf695228906ce48e1a85
Paul Colomiets <paul@colomiets.name>
2020-04-30 18:39:55 +0300

    Add "dev_mode" feature, and `PSQL_DEFAULT_PATH`
    These are used by edgedb build in dev mode
    
    Fixes #32

--- src/commands/psql.rs
+++ src/commands/psql.rs
@@ -1,3 +1,6 @@
+use std::env;
+use std::path::Path;
+use std::ffi::OsString;
 use std::process::Command;
 
 use anyhow::Context;
@@ -12,6 +15,31 @@ pub async fn psql<'x>(cli: &mut Client<'x>, _options: &Options)
     match cli.params.get::<PostgresAddress>() {
         Some(addr) => {
             let mut cmd = Command::new("psql");
+            #[cfg(all(feature="dev_mode", unix))]
+            {
+                if let Some(dir) = option_env!("PSQL_DEFAULT_PATH") {
+                    let psql_path = Path::new(dir).join("psql");
+                    if !psql_path.exists() {
+                        eprintln!("WARNING: {} does not exists",
+                                  psql_path.display());
+                    }
+                    let npath = if let Some(path) = env::var_os("PATH") {
+                        if path.is_empty() {
+                            dir.into()
+                        } else {
+                            let mut npath = OsString::with_capacity(
+                                dir.len() + 1 + path.len() + 1);
+                            npath.push(dir);
+                            npath.push(":");
+                            npath.push(path);
+                            npath
+                        }
+                    } else {
+                        dir.into()
+                    };
+                    cmd.env("PATH", npath);
+                }
+            }
             cmd.arg("-h").arg(&addr.host);
             cmd.arg("-U").arg(&addr.user);
             cmd.arg("-p").arg(addr.port.to_string());
