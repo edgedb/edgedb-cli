b1529cce8ae35512fcc9f444da8d399fa8574d8f
Paul Colomiets <paul@colomiets.name>
2020-06-02 17:39:35 +0300

    Eliminate most crashy TODOs

--- src/server/unknown_os.rs
+++ src/server/unknown_os.rs
@@ -2,6 +2,8 @@ use serde::Serialize;
 
 use crate::server::methods::{InstallationMethods, InstallMethod};
 use crate::server::os_trait::{CurrentOs, Method};
+use crate::server::docker::DockerCandidate;
+use crate::server::package::PackageCandidate;
 
 
 #[derive(Debug, Serialize)]
@@ -13,16 +15,30 @@ impl CurrentOs for Unknown {
     fn get_available_methods(&self)
         -> Result<InstallationMethods, anyhow::Error>
     {
-        todo!();
+        Ok(InstallationMethods {
+            package: PackageCandidate {
+                supported: false,
+                distro_name: "<unknown>".into(),
+                distro_version: "<unknown>".into(),
+                distro_supported: false,
+                version_supported: false,
+            },
+            docker: DockerCandidate::detect()?,
+        })
     }
     fn detect_all(&self) -> serde_json::Value {
         serde_json::to_value(self).expect("can serialize")
     }
-    fn make_method<'x>(&'x self, _method: &InstallMethod,
-        _methods: &InstallationMethods)
+    fn make_method<'x>(&'x self, method: &InstallMethod,
+        methods: &InstallationMethods)
         -> anyhow::Result<Box<dyn Method + 'x>>
     {
-        todo!();
+        use InstallMethod::*;
+        match method {
+            Package => anyhow::bail!(
+                "Package method is unsupported on current OS"),
+            Docker => Ok(Box::new(methods.docker.make_method(self)?)),
+        }
     }
 }
 
