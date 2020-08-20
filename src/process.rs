8ff14875052665aed812489b315826e888216110
Paul Colomiets <paul@colomiets.name>
2020-08-20 18:25:22 +0300

    Move `ProcessGuard` to `process` module

--- src/process.rs
+++ src/process.rs
@@ -1,6 +1,11 @@
-use std::process::{Command, exit};
+use std::io;
+use std::process::{Command, Child, exit};
 
 use anyhow::Context;
+
+pub struct ProcessGuard {
+    child: Child,
+}
 
 
 pub fn run(cmd: &mut Command) -> anyhow::Result<()> {
@@ -27,4 +32,34 @@ pub fn get_text(cmd: &mut Command) -> anyhow::Result<String> {
     };
     String::from_utf8(data)
         .context(format!("can decode output of {:?}", cmd))
+}
+
+impl ProcessGuard {
+    pub fn run(cmd: &mut Command) -> anyhow::Result<ProcessGuard> {
+        Ok(ProcessGuard {
+            child: cmd.spawn()?,
+        })
+    }
+}
+
+impl Drop for ProcessGuard {
+    fn drop(&mut self) {
+        #[cfg(unix)] {
+            let pid = self.child.id() as i32;
+            if unsafe { libc::kill(pid, libc::SIGTERM) } != 0 {
+                log::error!("error stopping server: {}",
+                    io::Error::last_os_error());
+            }
+        }
+        if cfg!(not(unix)) {
+            self.child.kill().map_err(|e| {
+                log::error!("error stopping server: {}", e);
+            }).ok();
+        }
+
+        // TODO(tailhook) figure out what signals and exit codes are okay
+        self.child.wait().map_err(|e| {
+            log::error!("error waiting for stopped server: {}", e);
+        }).ok();
+    }
 }
