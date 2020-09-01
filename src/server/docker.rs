bf7b3935302f0c337c33a6e494eb6a6de1d96822
Paul Colomiets <paul@colomiets.name>
2020-09-01 17:43:16 +0300

    docker: implement Method::installed_versions

--- src/server/docker.rs
+++ src/server/docker.rs
@@ -6,7 +6,7 @@ use anyhow::Context;
 use async_std::task;
 use serde::{Serialize, Deserialize};
 
-use crate::process::run;
+use crate::process;
 use crate::server::detect::Lazy;
 use crate::server::detect::VersionQuery;
 use crate::server::distribution::{DistributionRef, Distribution, MajorVersion};
@@ -91,6 +91,20 @@ impl Tag {
             _ => false,
         }
     }
+    pub fn into_distr(self) -> DistributionRef {
+        match &self {
+            Tag::Stable(v, rev) => Image {
+                major_version: MajorVersion::Stable(Version(v.clone())),
+                version: Version(format!("{}-{}", v, rev)),
+                tag: self,
+            },
+            Tag::Nightly(v) => Image {
+                major_version: MajorVersion::Nightly,
+                version: Version(v.clone()),
+                tag: self,
+            },
+        }.into_ref()
+    }
 }
 
 impl Distribution for Image {
@@ -160,6 +174,26 @@ impl DockerCandidate {
     }
 }
 
+impl Tag {
+    fn from_pair(name: &str, digest: &str) -> Option<Tag> {
+        if name.starts_with("1") {
+            // examples: `1-alpha4`, `1.1`
+            // digest example: sha256:0d56a04fe70892...
+            let rev = digest.split(":").skip(1).next();
+            match rev {
+                Some(rev) => Some(Tag::Stable(name.into(), rev[..7].into())),
+                None => None,
+            }
+        } else if name.starts_with("202") {
+            // example: `20200826052156c04ba5`
+            Some(Tag::Nightly(name.into()))
+        } else {
+            // maybe `latest` or something unrelated
+            None
+        }
+    }
+}
+
 impl<'os, O: CurrentOs + ?Sized> DockerMethod<'os, O> {
     fn get_tags(&self) -> anyhow::Result<&[Tag]> {
         self.tags.get_or_try_init(|| {
@@ -173,27 +207,13 @@ impl<'os, O: CurrentOs + ?Sized> DockerMethod<'os, O> {
                         "failed to fetch tag list from the Docker registry"
                     ).await?;
                     let last_page = data.results.len() < 1000;
-                    tags.extend(data.results.into_iter().flat_map(|t| {
-                        if t.name.starts_with("1") {
-                            // examples: `1-alpha4`, `1.1`
-                            let rev = t.images.get(0)
-                                .and_then(|img| {
-                                    img.digest.split(":").skip(1).next()
-                                });
-                            match rev {
-                                Some(rev) => {
-                                    Some(Tag::Stable(t.name, rev[..7].into()))
-                                }
-                                None => None,
-                            }
-                        } else if t.name.starts_with("202") {
-                            // example: `20200826052156c04ba5`
-                            Some(Tag::Nightly(t.name))
-                        } else {
-                            // maybe `latest` or something unrelated
-                            None
-                        }
-                    }));
+                    tags.extend(data.results
+                        .into_iter()
+                        .flat_map(|t| {
+                            t.images.get(0).and_then(|img| {
+                                Tag::from_pair(&t.name, &img.digest)
+                            })
+                        }));
                     if last_page {
                         break;
                     }
@@ -214,7 +234,7 @@ impl<'os, O: CurrentOs + ?Sized> Method for DockerMethod<'os, O> {
     {
         let image = settings.distribution.downcast_ref::<Image>()
             .context("invalid distribution for Docker")?;
-        run(Command::new(&self.cli)
+        process::run(Command::new(&self.cli)
             .arg("image")
             .arg("pull")
             .arg(format!("edgedb/edgedb:{}", match &image.tag {
@@ -262,21 +282,31 @@ impl<'os, O: CurrentOs + ?Sized> Method for DockerMethod<'os, O> {
             .filter(|tag| tag.matches(query))
             .max()
             .with_context(|| format!("version {} not found", query))?;
-        Ok(match tag {
-            Tag::Stable(v, rev) => Image {
-                major_version: MajorVersion::Stable(Version(v.clone())),
-                version: Version(format!("{}-{}", v, rev)),
-                tag: tag.clone(),
-            },
-            Tag::Nightly(v) => Image {
-                major_version: MajorVersion::Nightly,
-                version: Version(v.clone()),
-                tag: tag.clone(),
-            },
-        }.into_ref())
+        Ok(tag.clone().into_distr())
     }
     fn installed_versions(&self) -> anyhow::Result<Vec<DistributionRef>> {
-        Ok(Vec::new())
+        let data = process::get_text(Command::new(&self.cli)
+            .arg("image")
+            .arg("list")
+            .arg("--no-trunc")
+            .arg("--format")
+            .arg("{{.Repository}} {{.Tag}} {{.Digest}}"))?;
+        let mut result = Vec::new();
+        for line in data.lines() {
+            let mut words = line.split_whitespace();
+            if words.next() != Some("edgedb/edgedb") {
+                continue;
+            }
+            match (words.next(), words.next()) {
+                (Some(name), Some(digest)) => {
+                    if let Some(tag) = Tag::from_pair(name, digest) {
+                        result.push(tag.into_distr());
+                    }
+                }
+                _ => {}
+            }
+        }
+        Ok(result)
     }
     fn detect_all(&self) -> serde_json::Value {
         serde_json::to_value(self).expect("can serialize")
