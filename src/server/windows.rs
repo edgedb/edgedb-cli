0906d549e7a1abc57231b1c6c758dde083f4fa6f
Paul Colomiets <paul@colomiets.name>
2020-09-24 18:26:57 +0300

    Docker for windows is activated

--- src/server/windows.rs
+++ src/server/windows.rs
@@ -2,6 +2,8 @@ use crate::server::methods::{InstallationMethods, InstallMethod};
 use crate::server::os_trait::{CurrentOs, Method};
 
 use serde::Serialize;
+use crate::server::docker::DockerCandidate;
+use crate::server::package::{PackageCandidate};
 
 
 #[derive(Debug, Serialize)]
@@ -13,16 +15,30 @@ impl CurrentOs for Windows {
     fn get_available_methods(&self)
         -> Result<InstallationMethods, anyhow::Error>
     {
-        anyhow::bail!("Windows support is unimplemented yet"); // TODO
+        Ok(InstallationMethods {
+            package: PackageCandidate {
+                supported: false,
+                distro_name: "Windows".into(),
+                distro_version: "".into(), // TODO(tailhook)
+                distro_supported: false,
+                version_supported: false,
+            },
+            docker: DockerCandidate::detect()?,
+        })
     }
     fn detect_all(&self) -> serde_json::Value {
         serde_json::to_value(self).expect("can serialize")
     }
-    fn make_method<'x>(&'x self, _method: &InstallMethod,
-        _methods: &InstallationMethods)
+    fn make_method<'x>(&'x self, method: &InstallMethod,
+        methods: &InstallationMethods)
         -> anyhow::Result<Box<dyn Method + 'x>>
     {
-        anyhow::bail!("Windows support is unimplemented yet"); // TODO
+        use InstallMethod::*;
+
+        match method {
+            Package => anyhow::bail!("Method `package` is not supported"),
+            Docker => Ok(Box::new(methods.docker.make_method(self)?)),
+        }
     }
 }
 
