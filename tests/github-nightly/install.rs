892439ed1de3f1b01114358901c8d7075ed2edd3
Paul Colomiets <paul@colomiets.name>
2020-10-09 15:20:24 +0300

    Implement minimal upgrade test for docker

--- tests/github-nightly/install.rs
+++ tests/github-nightly/install.rs
@@ -19,7 +19,7 @@ fn package(tagname: &str, dockerfile: &str) -> anyhow::Result<()> {
         .add_bin()?;
     build_image(context, tagname)?;
     run(tagname, r###"
-        edgedb server install --version=1-alpha5
+        edgedb server install
     "###).success();
     Ok(())
 }
@@ -41,20 +41,8 @@ fn docker(tagname: &str, dockerfile: &str) -> anyhow::Result<()> {
         docker ps -q -f 'name=edgedb_test' | xargs -r docker container kill
         docker system prune --all --force
         docker volume list -q -f 'name=edgedb_test' | xargs -r docker volume rm
-        mkdir /tmp/workdir
-        cd /tmp/workdir
 
-        edgedb server install --version=1-alpha6 --method=docker
-        edgedb server init test1
-        edgedb --wait-until-available=30s -Itest1 query '
-            CREATE TYPE Type1 {
-                CREATE PROPERTY prop1 -> str;
-            }
-        ' 'INSERT Type1 { prop1 := "value1" }'
-        edgedb server upgrade test1 --to-nightly
-        val=$(edgedb -Itest1 --wait-until-available=30s --tab-separated \
-              query 'SELECT Type1 { prop1 }')
-        test "$val" = "value1"
+        edgedb server install --nightly --method=docker
     "###).success();
     Ok(())
 }
