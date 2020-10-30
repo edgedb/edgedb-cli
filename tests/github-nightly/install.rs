21df6161c6b360ec7047b44c420114804bd8d4f3
Paul Colomiets <paul@colomiets.name>
2020-10-30 15:12:50 +0200

    Fixing tests, adding nightly install tests

--- tests/github-nightly/install.rs
+++ tests/github-nightly/install.rs
@@ -62,12 +62,59 @@ fn docker(tagname: &str, dockerfile: &str) -> anyhow::Result<()> {
         .add_bin()?;
     build_image(context, tagname)?;
     run_docker(tagname, r###"
-        edgedb server install --nightly --method=docker
+        edgedb server install --method=docker
         edgedb server init test1
         val=$(edgedb -Itest1 --wait-until-available=30s query "SELECT 1+1")
         test "$val" = "2"
         edgedb server logs test1
         edgedb server uninstall --all --verbose
+    "###).success();
+    Ok(())
+}
+
+#[test_case("edbtest_bionic", &dock_ubuntu("bionic"))]
+#[test_case("edbtest_xenial", &dock_ubuntu("xenial"))]
+#[test_case("edbtest_focal", &dock_ubuntu("focal"))]
+#[test_case("edbtest_centos8", &dock_centos(8))]
+#[test_case("edbtest_buster", &dock_debian("buster"))]
+#[test_case("edbtest_stretch", &dock_debian("stretch"))]
+fn package_nighlty(tagname: &str, dockerfile: &str)
+    -> anyhow::Result<()>
+{
+    let context = Context::new()
+        .add_file("Dockerfile", dockerfile)?
+        .add_sudoers()?
+        .add_bin()?;
+    build_image(context, tagname)?;
+    run_systemd(tagname, r###"
+        edgedb server install --nightly
+        edgedb server init test1 --nightly
+        val=$(edgedb -Itest1 --wait-until-available=30s query "SELECT 1+1")
+        test "$val" = "2"
+        edgedb server logs test1
+    "###).success();
+    Ok(())
+}
+
+#[test_case("edbtest_bionic", &dock_ubuntu("bionic"))]
+#[test_case("edbtest_xenial", &dock_ubuntu("xenial"))]
+#[test_case("edbtest_focal", &dock_ubuntu("focal"))]
+#[test_case("edbtest_centos7", &dock_centos(7))]
+#[test_case("edbtest_centos8", &dock_centos(8))]
+#[test_case("edbtest_buster", &dock_debian("buster"))]
+#[test_case("edbtest_stretch", &dock_debian("stretch"))]
+fn docker_nightly(tagname: &str, dockerfile: &str) -> anyhow::Result<()> {
+    let context = Context::new()
+        .add_file("Dockerfile", dockerfile)?
+        .add_sudoers()?
+        .add_bin()?;
+    build_image(context, tagname)?;
+    run_docker(tagname, r###"
+        edgedb server install --method=docker --nightly
+        edgedb server init test1 --nightly
+        val=$(edgedb -Itest1 --wait-until-available=30s query "SELECT 1+1")
+        test "$val" = "2"
+        edgedb server logs test1
     "###).success();
     Ok(())
 }
