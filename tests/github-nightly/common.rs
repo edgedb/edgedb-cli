1294413786479557d08901482189e53f2e0ca088
Paul Colomiets <paul@colomiets.name>
2020-11-04 11:28:55 +0200

    nightly-test: Add JS binding test, test py only on focal

--- tests/github-nightly/common.rs
+++ tests/github-nightly/common.rs
@@ -2,7 +2,31 @@ pub fn dock_ubuntu(codename: &str) -> String {
     format!(r###"
         FROM ubuntu:{codename}
         ENV DEBIAN_FRONTEND=noninteractive
-        RUN apt-get update && apt-get install -y ca-certificates sudo gnupg2 apt-transport-https curl software-properties-common dbus-user-session python3-pip
+        RUN apt-get update && apt-get install -y \
+            ca-certificates sudo gnupg2 apt-transport-https curl \
+            software-properties-common dbus-user-session
+        RUN curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+        RUN add-apt-repository \
+           "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+           $(lsb_release -cs) \
+           stable"
+        RUN apt-get update && apt-get install -y docker-ce-cli
+        RUN adduser --uid 1000 --home /home/user1 \
+            --shell /bin/bash --ingroup users --gecos "EdgeDB Test User" \
+            user1
+        ADD ./edgedb /usr/bin/edgedb
+        ADD ./sudoers /etc/sudoers
+    "###, codename=codename)
+}
+
+pub fn dock_ubuntu_jspy(codename: &str) -> String {
+    format!(r###"
+        FROM ubuntu:{codename}
+        ENV DEBIAN_FRONTEND=noninteractive
+        RUN apt-get update && apt-get install -y \
+            ca-certificates sudo gnupg2 apt-transport-https curl \
+            software-properties-common dbus-user-session \
+            python3-pip npm
         RUN curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
         RUN add-apt-repository \
            "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -13,16 +37,18 @@ pub fn dock_ubuntu(codename: &str) -> String {
             --shell /bin/bash --ingroup users --gecos "EdgeDB Test User" \
             user1
         RUN pip3 install edgedb
+        RUN npm install edgedb
         ADD ./edgedb /usr/bin/edgedb
         ADD ./sudoers /etc/sudoers
         ADD ./edbconnect.py /usr/bin/edbconnect.py
+        ADD ./edbconnect.js /usr/bin/edbconnect.js
     "###, codename=codename)
 }
 
 pub fn dock_centos(codename: u32) -> String {
     format!(r###"
         FROM centos:{codename}
-        RUN yum -y install sudo yum-utils systemd python3-pip
+        RUN yum -y install sudo yum-utils systemd
         RUN yum-config-manager \
             --add-repo \
             https://download.docker.com/linux/centos/docker-ce.repo
@@ -30,10 +56,8 @@ pub fn dock_centos(codename: u32) -> String {
         RUN adduser --uid 1000 --home /home/user1 \
             --shell /bin/bash --group users \
             user1
-        RUN pip3 install edgedb
         ADD ./edgedb /usr/bin/edgedb
         ADD ./sudoers /etc/sudoers
-        ADD ./edbconnect.py /usr/bin/edbconnect.py
     "###, codename=codename)
 }
 
@@ -41,7 +65,9 @@ pub fn dock_debian(codename: &str) -> String {
     format!(r###"
         FROM debian:{codename}
         ENV DEBIAN_FRONTEND=noninteractive
-        RUN apt-get update && apt-get install -y ca-certificates sudo gnupg2 apt-transport-https curl software-properties-common dbus-user-session python3-pip
+        RUN apt-get update && apt-get install -y \
+            ca-certificates sudo gnupg2 apt-transport-https curl \
+            software-properties-common dbus-user-session
         RUN curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
         RUN add-apt-repository \
            "deb [arch=amd64] https://download.docker.com/linux/debian \
@@ -51,9 +77,7 @@ pub fn dock_debian(codename: &str) -> String {
         RUN adduser --uid 1000 --home /home/user1 \
             --shell /bin/bash --ingroup users --gecos "EdgeDB Test User" \
             user1
-        RUN pip3 install edgedb
         ADD ./edgedb /usr/bin/edgedb
         ADD ./sudoers /etc/sudoers
-        ADD ./edbconnect.py /usr/bin/edbconnect.py
     "###, codename=codename)
 }
