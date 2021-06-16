042143bbea30cf375e19a378fdbe6f7fe39161ac
Paul Colomiets <paul@colomiets.name>
2021-06-16 16:02:42 +0300

    repl: Codegen for settings

--- edgedb-cli-derive/src/into_app.rs
+++ edgedb-cli-derive/src/into_app.rs
@@ -102,6 +102,7 @@ pub fn subcommands(e: &types::Enum) -> TokenStream {
     let from_sub = mk_match_subcommand(&e, &sub);
     let propagation = mk_subcommand_propagation(&e);
     let describe_subcommands = mk_subcommand_describe(&e);
+    let setting = if e.attrs.setting { mk_setting_impl(&e) } else { quote!() };
     quote! {
         impl #impl_gen clap::Subcommand for #ident #ty_gen #where_cl {
             fn from_subcommand(subcommand: Option<(&str, &clap::ArgMatches)>)
@@ -138,6 +139,7 @@ pub fn subcommands(e: &types::Enum) -> TokenStream {
             }
         }
         #propagation
+        #setting
     }
 }
 
@@ -843,6 +845,54 @@ fn mk_subcommand_describe(e: &types::Enum) -> TokenStream {
                 #( #direct ),*
             ];
             return SUBCOMMANDS;
+        }
+    }
+}
+
+fn mk_setting_impl(e: &types::Enum) -> TokenStream {
+    let ref ident = e.ident;
+    let (impl_gen, ty_gen, where_cl) = e.generics.split_for_impl();
+    let to_string = e.subcommands.iter().map(|sub| {
+        let variant = &sub.ident;
+        let name = ::heck::KebabCase::to_kebab_case(&variant.to_string()[..]);
+        quote! {
+            #ident::#variant(..) => #name
+        }
+    });
+    let is_show = e.subcommands.iter().map(|sub| {
+        let variant = &sub.ident;
+        quote! {
+            #ident::#variant(val) => val.value.is_none()
+        }
+    });
+    let all_items = e.subcommands.iter().map(|sub| {
+        let variant = &sub.ident;
+        quote! {
+            #ident::#variant(::std::default::Default::default())
+        }
+    });
+    quote! {
+        impl #impl_gen #ident #ty_gen #where_cl
+        {
+            pub fn name(&self) -> &'static str {
+                match self {
+                    #( #to_string ),*
+                }
+            }
+            pub fn is_show(&self) -> bool {
+                use Setting::*;
+
+                match self {
+                    #( #is_show ),*
+                }
+            }
+            pub fn all_items() -> &'static [#ident] {
+                static SETTINGS: ::once_cell::sync::OnceCell<Vec<#ident>>
+                    = ::once_cell::sync::OnceCell::new();
+                return &SETTINGS.get_or_init(|| {
+                    vec![#( #all_items ),*]
+                })[..];
+            }
         }
     }
 }
