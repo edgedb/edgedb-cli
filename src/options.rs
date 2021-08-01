ab9d4a251656bfdb3ced5066aa07e452b43cb2e9
Fantix King <fantix.king@gmail.com>
2021-08-01 15:01:15 -0400

    Support old command with warning

--- src/options.rs
+++ src/options.rs
@@ -343,6 +343,23 @@ fn get_deprecated_matches(mismatch_cmd: &str) -> Option<clap::ArgMatches> {
             }
             _ => return None,
         }
+        Some(first_cmd) if first_cmd == "self" => match args.next() {
+            Some(second_cmd) if first_cmd == mismatch_cmd => {
+                if let Ok(second_cmd) = second_cmd.into_string() {
+                    old_name = format!("self {}", second_cmd);
+                    skip = 3;
+                    match second_cmd.as_str() {
+                        "install" => "cli install",
+                        "migrate" => "cli migrate",
+                        "upgrade" => "cli upgrade",
+                        _ => return None,
+                    }
+                } else {
+                    return None;
+                }
+            }
+            _ => return None,
+        }
         Some(first_cmd) if first_cmd == mismatch_cmd => {
             old_name = mismatch_cmd.into();
             skip = 2;
