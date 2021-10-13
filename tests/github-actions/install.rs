05f8e427884f7375fb2f3228f69b9bb23ec75285
Paul Colomiets <paul@colomiets.name>
2021-10-13 17:18:24 +0300

    tests: improve logging in github-actions-test

--- tests/github-actions/install.rs
+++ tests/github-actions/install.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::io::Write;
 
-use assert_cmd::Command;
+use assert_cmd::{Command, assert::Assert};
 use async_std::prelude::FutureExt;
 use dirs::home_dir;
 use predicates::boolean::PredicateBooleanExt;
@@ -13,6 +13,23 @@ use crate::certs::Certs;
 
 const UNIX_INST: &str =
     "curl --proto '=https' --tlsv1.2 -sSf https://localhost:8443 | sh -s -- -y";
+
+trait OutputExt {
+    fn context(self, name: &'static str, description: &'static str) -> Self;
+}
+
+impl OutputExt for Assert {
+    fn context(mut self, name: &'static str, description: &'static str) -> Self
+    {
+        self = self.append_context(name, description);
+        let out = self.get_output();
+        println!("------ {}: {} (STDOUT) -----", name, description);
+        println!("{}", String::from_utf8_lossy(&out.stdout));
+        println!("------ {}: {} (STDERR) -----", name, description);
+        println!("{}", String::from_utf8_lossy(&out.stderr));
+        self
+    }
+}
 
 #[test]
 fn github_action_install() -> anyhow::Result<()> {
@@ -56,6 +73,7 @@ fn github_action_install() -> anyhow::Result<()> {
         Command::new("edgedb-init.exe")
             .arg("-y")
             .assert()
+            .context("edgedb-init", "self install by command name")
             .success();
     } else {
         let mut tmpfile = tempfile::NamedTempFile::new()?;
@@ -67,6 +85,7 @@ fn github_action_install() -> anyhow::Result<()> {
             .env("CURL_CA_BUNDLE", tmpfile.path())
             .env("EDGEDB_PKG_ROOT", "https://localhost:8443")
             .assert()
+            .context("curl shebang", "command-line install")
             .success()
             .stdout(predicates::str::contains(
                 "The EdgeDB command-line tool is now installed"));
@@ -86,174 +105,183 @@ fn github_action_install() -> anyhow::Result<()> {
     Command::new(&edgedb)
         .arg("--version")
         .assert()
+        .context("version", "command-line version option")
         .success()
         .stdout(predicates::str::contains(
             concat!("EdgeDB CLI ", env!("CARGO_PKG_VERSION"))));
 
     if !cfg!(windows) {
-        println!("Install");
         Command::new(&edgedb)
             .arg("server").arg("install")
             .assert()
+            .context("install", "server installation")
             .success();
 
         // TODO(tailhook) check output somehow
-        println!("List versions");
         Command::new(&edgedb)
             .arg("server").arg("list-versions")
             .assert()
+            .context("list-versions", "list versions of the server")
             .success();
 
         // Extra install fails with code 51
-        println!("Conflict on installing again");
         Command::new(&edgedb)
             .arg("server").arg("install")
             .assert()
+            .context("install-2", "check that installation conficts")
             .code(51);
 
-        println!("Install different version");
         Command::new(&edgedb)
             .arg("server").arg("install").arg("--version=1-beta3")
             .assert()
+            .context("install-old", "older version of edgedb")
             .success();
 
         Command::new(&edgedb)
             .arg("server").arg("list-versions")
             .arg("--installed-only").arg("--column=major-version")
             .assert()
+            .context("installed only", "check the version is installed")
             .success()
             .stdout(predicates::str::contains("1-beta3"));
 
         if cfg!(target_os="macos") {
-            println!("Init first");
             Command::new(&edgedb)
                 .arg("instance").arg("create").arg("inst1")
                 .assert()
+                .context("create-1", "created `inst1`")
                 .success();
 
-            println!("Execute query");
+            Command::new(&edgedb)
+                .arg("instance").arg("logs").arg("inst1")
+                .assert()
+                .context("log-1-0", "logs of `inst1`")
+                .success();
+
             Command::new(&edgedb)
                 .arg("--admin").arg("--instance").arg("inst1")
                 .arg("--wait-until-available=20s")
                 .arg("query").arg("SELECT 1")
                 .assert()
+                .context("query-1", "query `inst1` first time")
                 .success();
 
-            println!("Status");
             Command::new(&edgedb)
                 .arg("instance").arg("status").arg("inst1")
                 .assert()
+                .context("status-1", "status `inst1` first time")
                 .success();
 
-            println!("Restart");
             Command::new(&edgedb)
                 .arg("instance").arg("restart").arg("inst1")
                 .assert()
+                .context("restart-1", "restart `inst1`")
                 .success();
 
-            println!("Status");
             Command::new(&edgedb)
                 .arg("instance").arg("status").arg("inst1")
                 .assert()
+                .context("status-1-1", "status `inst1` after restart")
                 .success();
 
-            println!("Stop");
             Command::new(&edgedb)
                 .arg("instance").arg("stop").arg("inst1")
                 .assert()
+                .context("stop-1", "stop `inst1`")
                 .success();
 
-            println!("Status");
             Command::new(&edgedb)
                 .arg("instance").arg("status").arg("inst1")
                 .assert()
+                .context("status-1-2", "status `inst1` after stop")
                 .code(3);
 
-            println!("Init second one");
             Command::new(&edgedb)
                 .arg("instance").arg("create").arg("second")
                     .arg("--version=1-beta3")
                 .assert()
+                .context("create-2", "create `second`")
                 .success();
 
-            println!("Start second");
             Command::new(&edgedb)
                 .arg("instance").arg("start").arg("second")
                 .assert()
+                .context("start-2", "start `second`")
                 .success();
 
-            println!("Start first simultaneously to second");
             Command::new(&edgedb)
                 .arg("instance").arg("start").arg("inst1")
                 .assert()
+                .context("start-1-3", "start `inst1` again")
                 .success();
 
-            println!("Status second");
             Command::new(&edgedb)
                 .arg("instance").arg("status").arg("second")
                 .assert()
+                .context("status-2", "status `second`")
                 .success();
 
-            println!("Logs inst1");
             Command::new(&edgedb)
                 .arg("instance").arg("logs").arg("inst1")
                 .assert()
+                .context("log-1", "logs of `inst1`")
                 .success();
 
-            println!("Logs second");
             Command::new(&edgedb)
                 .arg("instance").arg("logs").arg("second")
                 .assert()
+                .context("log-2", "logs of `second`")
                 .success();
 
-            println!("Status first");
             Command::new(&edgedb)
                 .arg("instance").arg("status").arg("inst1")
                 .assert()
+                .context("status-1-4", "status of `inst1`")
                 .success();
 
-            println!("Upgrading");
             Command::new(&edgedb)
                 .arg("instance").arg("upgrade").arg("inst1")
                 .arg("--to-latest").arg("--force")
                 .assert()
+                .context("upgrade-1", "force upgrade `inst1` to latest")
                 .success();
 
-            println!("Execute query after upgrade");
             Command::new(&edgedb)
                 .arg("--admin").arg("--instance").arg("inst1")
                 .arg("--wait-until-available=20s")
                 .arg("query").arg("SELECT 1")
                 .assert()
+                .context("query-1-2", "query `inst1` after upgrade")
                 .success();
 
-            println!("Delete second instance");
             Command::new(&edgedb)
                 .arg("instance").arg("destroy").arg("second")
                 .assert()
+                .context("destroy-2", "destroy `second` instance")
                 .success();
 
         }
 
-        println!("Uninstall the old version");
         Command::new(&edgedb)
-            .arg("server").arg("uninstall").arg("--version=1-beta2")
+            .arg("server").arg("uninstall").arg("--version=1-beta3")
             .assert()
+            .context("uninstall-2", "uninstall old version")
             .success();
         Command::new(&edgedb)
             .arg("server").arg("list-versions")
             .arg("--installed-only").arg("--column=major-version")
             .assert()
             .success()
+            .context("list-2", "list after uninstall")
             .stdout(predicates::str::contains("1-beta2").not());
 
         if cfg!(target_os="macos") {
-            println!("Execute query after deleting second");
             Command::new(&edgedb)
                 .arg("--admin").arg("--instance").arg("inst1")
                 .arg("--wait-until-available=20s")
                 .arg("query").arg("SELECT 1")
                 .assert()
+                .context("query-1a", "late query of `inst1`")
                 .success();
         }
     }
