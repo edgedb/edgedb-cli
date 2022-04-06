018d38abc6bea6303d7eda9cdc4c3199d721f3cd
Paul Colomiets <paul@edgedb.com>
2022-04-06 15:27:49 +0300

    Adding `EDGEDB_RUN_VERSION_CHECK=strict`

--- tests/func/non_interactive.rs
+++ tests/func/non_interactive.rs
@@ -1,4 +1,5 @@
 use crate::SERVER;
+use assert_cmd::Command;
 
 
 #[test]
@@ -6,4 +7,14 @@ fn with_comment() {
     SERVER.admin_cmd()
         .write_stdin("SELECT 1; # comment")
         .assert().success();
+}
+
+#[test]
+fn strict_version_check() {
+    Command::cargo_bin("edgedb")
+        .expect("binary found")
+        .env("EDGEDB_RUN_VERSION_CHECK", "strict")
+        .arg("info")
+        .assert()
+        .success();
 }
