a3aefe055b7d8e8e12605b78a99bff75ba09730a
Nik Sidnev <sidnev.nick@gmail.com>
2022-05-31 17:54:37 +0300

    add tests

--- tests/portable_project_dir.rs
+++ tests/portable_project_dir.rs
@@ -9,7 +9,7 @@ use util::*;
 
 #[cfg(feature="portable_tests")]
 #[test]
-fn project_link_and_init() {
+fn project_link_and_init_from_non_project_dir() {
     Command::new("edgedb")
         .arg("--version")
         .assert()
@@ -27,14 +27,14 @@ fn project_link_and_init() {
     Command::new("edgedb")
         .arg("instance").arg("create").arg("inst1")
         .assert()
-        .context("create-1", "created `inst1`")
+        .context("proj-dir-create-1", "created `inst1`")
         .success();
 
     Command::new("edgedb")
         .arg("project").arg("info").arg("--instance-name")
-        .current_dir("tests/proj/project1")
+        .arg("--project-dir=tests/proj/project1")
         .assert()
-        .context("project-info-no", "not initialied")
+        .context("proj-dir-project-info-no", "not initialied")
         .code(1)
         .stderr(predicates::str::contains("is not initialized"));
 
@@ -42,16 +42,16 @@ fn project_link_and_init() {
         .arg("project").arg("init").arg("--link")
         .arg("--server-instance=inst1")
         .arg("--non-interactive")
-        .current_dir("tests/proj/project1")
+        .arg("--project-dir=tests/proj/project1")
         .assert()
-        .context("project-link", "linked `inst1` to project project1")
+        .context("proj-dir-project-link", "linked `inst1` to project project1")
         .success();
 
     Command::new("edgedb")
         .arg("project").arg("info").arg("--instance-name")
-        .current_dir("tests/proj/project1")
+        .arg("--project-dir=tests/proj/project1")
         .assert()
-        .context("project-info", "instance-name == inst1")
+        .context("proj-dir-project-info", "instance-name == inst1")
         .success()
         .stdout(predicates::ord::eq("inst1\n"));
 
@@ -59,21 +59,21 @@ fn project_link_and_init() {
         .arg("query").arg("SELECT 1")
         .current_dir("tests/proj/project1")
         .assert()
-        .context("query-1", "query of project")
+        .context("proj-dir-query-1", "query of project")
         .success();
 
     Command::new("edgedb").arg("project").arg("init")
         .arg("--non-interactive")
-        .current_dir("tests/proj/project2")
+        .arg("--project-dir=tests/proj/project2")
         .assert()
-        .context("project-init", "init project2")
+        .context("proj-dir-project-init", "init project2")
         .success();
 
     Command::new("edgedb")
         .arg("project").arg("info").arg("--instance-name")
-        .current_dir("tests/proj/project2")
+        .arg("--project-dir=tests/proj/project2")
         .assert()
-        .context("project-info", "instance-name == project2")
+        .context("proj-dir-project-info", "instance-name == project2")
         .success()
         .stdout(predicates::ord::eq("project2\n"));
 
@@ -81,47 +81,47 @@ fn project_link_and_init() {
         .arg("query").arg("SELECT 1")
         .current_dir("tests/proj/project2")
         .assert()
-        .context("query-2", "query of project2")
+        .context("proj-dir-query-2", "query of project2")
         .success();
 
     Command::new("edgedb").arg("project").arg("upgrade")
         .arg("--force")
-        .current_dir("tests/proj/project2")
+        .arg("--project-dir=tests/proj/project2")
         .assert()
-        .context("project-upgrade", "upgrade project")
+        .context("proj-dir-project-upgrade", "upgrade project")
         .success();
 
     Command::new("edgedb")
         .arg("query").arg("SELECT 1")
         .current_dir("tests/proj/project2")
         .assert()
-        .context("query-3", "query after upgrade")
+        .context("proj-dir-query-3", "query after upgrade")
         .success();
 
     Command::new("edgedb")
         .arg("instance").arg("destroy").arg("project2")
         .arg("--non-interactive")
         .assert()
-        .context("destroy-2-no", "should warn")
+        .context("proj-dir-destroy-2-no", "should warn")
         .code(2);
 
     Command::new("edgedb")
         .arg("instance").arg("destroy").arg("inst1")
         .arg("--non-interactive")
         .assert()
-        .context("destroy-1-no", "should warn")
+        .context("proj-dir-destroy-1-no", "should warn")
         .code(2);
 
     Command::new("edgedb")
         .arg("instance").arg("destroy").arg("project1")
         .arg("--non-interactive")
         .assert()
-        .context("destroy-1-non-exist", "it's project name, not instance name")
+        .context("proj-dir-destroy-1-non-exist", "it's project name, not instance name")
         .code(8); // instance not found
 
     Command::new("edgedb").arg("instance").arg("list")
         .assert()
-        .context("instance-list-1", "list two instances")
+        .context("proj-dir-instance-list-1", "list two instances")
         .success()
         .stdout(predicates::str::contains("inst1"))
         .stdout(predicates::str::contains("project2"));
@@ -130,12 +130,12 @@ fn project_link_and_init() {
         .arg("instance").arg("destroy").arg("project2")
         .arg("--force")
         .assert()
-        .context("destroy-2", "should destroy")
+        .context("proj-dir-destroy-2", "should destroy")
         .success();
 
     Command::new("edgedb").arg("instance").arg("list")
         .assert()
-        .context("instance-list-2", "list once instance")
+        .context("proj-dir-instance-list-2", "list once instance")
         .success()
         .stdout(predicates::str::contains("inst1"))
         .stdout(predicates::str::contains("project2").not());
@@ -143,14 +143,14 @@ fn project_link_and_init() {
 
     Command::new("edgedb")
         .arg("project").arg("unlink").arg("-D").arg("--non-interactive")
-        .current_dir("tests/proj/project1")
+        .arg("--project-dir=tests/proj/project1")
         .assert()
-        .context("destroy-1", "should unlink and destroy project")
+        .context("proj-dir-destroy-1", "should unlink and destroy project")
         .success();
 
     Command::new("edgedb").arg("instance").arg("list")
         .assert()
-        .context("instance-list-3", "list no instances")
+        .context("proj-dir-instance-list-3", "list no instances")
         .success()
         .stdout(predicates::str::contains("inst1").not())
         .stdout(predicates::str::contains("project2").not());
@@ -158,34 +158,34 @@ fn project_link_and_init() {
     Command::new("edgedb").arg("project").arg("init")
         .arg("--non-interactive")
         .arg("--server-start-conf=manual")
-        .current_dir("tests/proj/project2")
+        .arg("--project-dir=tests/proj/project2")
         .assert()
-        .context("project-init-manual", "init project2 manual")
+        .context("proj-dir-project-init-manual", "init project2 manual")
         .success();
 
     Command::new("edgedb").arg("project").arg("upgrade")
         .arg("--to-latest").arg("--force")
-        .current_dir("tests/proj/project2")
+        .arg("--project-dir=tests/proj/project2")
         .assert()
-        .context("project-upgrade-manual", "upgrade manual project")
+        .context("proj-dir-project-upgrade-manual", "upgrade manual project")
         .success();
 
     Command::new("edgedb").arg("instance").arg("status").arg("project2")
         .arg("--extended")
         .assert()
-        .context("instance-status", "show extended status")
+        .context("proj-dir-instance-status", "show extended status")
         .code(3);
 
     Command::new("edgedb").arg("instance").arg("revert").arg("project2")
         .arg("--no-confirm")
         .assert()
-        .context("project-revert-manual", "revert manual project")
+        .context("proj-dir-project-revert-manual", "revert manual project")
         .success();
 
     Command::new("edgedb")
         .arg("project").arg("unlink").arg("-D").arg("--non-interactive")
-        .current_dir("tests/proj/project2")
+        .arg("--project-dir=tests/proj/project2")
         .assert()
-        .context("destroy-2", "should unlink and destroy project")
+        .context("proj-dir-destroy-2", "should unlink and destroy project")
         .success();
 }
