0c92bbb59f744f2d631c38b6ae807b07f1b50af7
Paul Colomiets <paul@edgedb.com>
2022-06-08 16:27:22 +0300

    Fixing centos8

--- tests/github-nightly/common.rs
+++ tests/github-nightly/common.rs
@@ -55,6 +55,10 @@ pub fn dock_centos(codename: u32) -> String {
     format!(
         r###"
         FROM centos:{codename}
+        RUN sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+        RUN sed -i 's|#baseurl=http://mirror.centos.org\
+                     |baseurl=http://vault.centos.org|g' \
+                     /etc/yum.repos.d/CentOS-*
         RUN yum -y install sudo yum-utils systemd
         RUN yum-config-manager \
             --add-repo \
