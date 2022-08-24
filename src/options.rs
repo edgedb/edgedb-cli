5552b61ae362457da384468e797459832bcb8fb6
Paul Colomiets <paul@edgedb.com>
2022-08-24 22:54:49 +0300

    Fix incomplete help for `--unix-path` (#828)

--- src/options.rs
+++ src/options.rs
@@ -80,7 +80,10 @@ pub struct ConnectionOptions {
     #[clap(conflicts_with_all=&["dsn", "credentials_file", "instance"])]
     pub port: Option<u16>,
 
-    /// Unix socket dir for the
+    /// A path to a unix socket for the EdgeDB connection
+    ///
+    /// When the supplied path is a directory, the actual path will be
+    /// computed using the `--port` and `--admin` parameters.
     #[clap(long, help_heading=Some(CONN_OPTIONS_GROUP))]
     #[clap(value_hint=ValueHint::AnyPath)]
     #[clap(hide=true)]
