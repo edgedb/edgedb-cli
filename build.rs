23e70107ea0076a9908d37e51ff5de4c6f24f924
Paul Colomiets <paul@edgedb.com>
2023-01-24 13:39:04 +0200

    Fix shared test cases

--- build.rs
+++ build.rs
@@ -57,7 +57,7 @@ fn main() {
         ("multiple_compound_opts", "(cannot be used with|provided more than once)"),
         ("multiple_compound_env", "multiple compound env vars found"),
         ("exclusive_options", "provided more than once"),
-        ("credentials_file_not_found", "credentials file.*could not read"),
+        ("credentials_file_not_found", "credentials file.*No such file"),
         ("project_not_initialised", "error reading project settings"),
         ("secret_key_not_found", "Run `edgedb cloud login`"),
         ("invalid_secret_key", "Illegal JWT token"),
