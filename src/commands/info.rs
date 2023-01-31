af6f21d80c5d0d8ee802c718fb402a872fc353c1
Paul Colomiets <paul@edgedb.com>
2023-01-31 12:16:18 +0200

    Made info command non-async

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -47,9 +47,7 @@ pub fn specific_info(item: &str) -> Result<(), anyhow::Error> {
     Ok(())
 }
 
-#[tokio::main]
-pub async fn info(_options: &Options, info: &Info)-> Result<(), anyhow::Error>
-{
+pub fn info(_options: &Options, info: &Info)-> Result<(), anyhow::Error> {
     if let Some(ref item) = info.get {
         return specific_info(&item);
     }
