c95ae8ac2e9ec1a2c3996cd3bcae0a3f63086f84
Paul Colomiets <paul@edgedb.com>
2023-03-20 13:21:09 +0200

    WIP Analyze implementation

---
+++ src/analyze/model.rs
@@ -0,0 +1,214 @@
+use std::fmt;
+use std::sync::Arc;
+
+use serde::Deserialize;
+
+
+pub type IndexCell = Arc<crossbeam_utils::atomic::AtomicCell<Option<u32>>>;
+
+
+#[derive(Deserialize, Debug)]
+pub struct Explain {
+    pub buffers: Vec<String>,
+    pub fine_grained: Option<Plan>,
+    pub coarse_grained: Option<Shape>,
+    pub debug_info: Option<DebugInfo>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct DebugInfo {
+    pub full_plan: Option<DebugNode>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(tag="type", content="value")]
+pub enum PropValue {
+    #[serde(rename="kB")]
+    Kbytes(u64),
+    #[serde(rename="ms")]
+    Millis(f64),
+    #[serde(rename="expr")]
+    Expr(String),
+    #[serde(rename="index")]
+    Index(String),
+    #[serde(rename="relation")]
+    Relation(String),
+    #[serde(rename="text")]
+    Text(String),
+    #[serde(rename="int")]
+    Int(i64),
+    #[serde(rename="float")]
+    Float(f64),
+
+    #[serde(rename="list:kB")]
+    ListKbytes(Vec<u64>),
+    #[serde(rename="list:ms")]
+    ListMillis(Vec<f64>),
+    #[serde(rename="list:expr")]
+    ListExpr(Vec<String>),
+    #[serde(rename="list:index")]
+    ListIndex(Vec<String>),
+    #[serde(rename="list:relation")]
+    ListRelation(Vec<String>),
+    #[serde(rename="list:text")]
+    ListText(Vec<String>),
+    #[serde(rename="list:int")]
+    ListInt(Vec<i64>),
+    #[serde(rename="list:float")]
+    ListFloat(Vec<f64>),
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Prop {
+    pub title: String,
+    pub important: bool,
+    #[serde(flatten)]
+    pub value: PropValue,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Stage {
+    pub plan_id: uuid::Uuid,
+    pub plan_type: String,
+    pub properties: Vec<Prop>,
+    #[serde(flatten)]
+    pub cost: Cost,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Plan {
+    pub alias: Option<String>,
+    #[serde(default)]
+    pub contexts: Vec<Context>,
+    pub pipeline: Vec<Stage>,
+    pub subplans: Vec<Plan>,
+}
+
+
+#[derive(Deserialize, Debug)]
+pub struct Cost {
+    pub plan_rows: u64,
+    pub plan_width: u64,
+    pub startup_cost: f32,
+    pub total_cost: f32,
+
+    pub actual_startup_time: Option<f32>,
+    pub actual_total_times: Option<f32>,
+    pub actual_rows: Option<f32>,
+    pub actual_loops: Option<f32>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(tag="kind")]
+pub enum ChildName {
+    #[serde(rename="pointer")]
+    Pointer { name: String },
+    #[serde(rename="filter")]
+    Filter,
+    #[serde(other)]
+    Other
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Child {
+    #[serde(flatten)]
+    pub name: ChildName,
+    pub node: Shape,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Shape {
+    pub plan_id: uuid::Uuid,
+    #[serde(default)]
+    pub children: Vec<Child>,
+    #[serde(default)]
+    pub contexts: Vec<Context>,
+    #[serde(default)]
+    pub relations: Vec<String>,
+    #[serde(flatten)]
+    pub cost: Cost,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Context {
+    pub start: usize,
+    pub end: usize,
+    pub buffer_idx: usize,
+    pub text: String,
+    #[serde(skip, default)]
+    pub index_cell: IndexCell,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct DebugNode {
+    pub node_type: String,
+    pub parent_relationship: Option<String>,
+    #[serde(default)]
+    pub plans: Vec<DebugNode>,
+    #[serde(default)]
+    pub is_shape: bool,
+    pub alias: Option<String>,
+    pub index_name: Option<String>,
+    pub relation_name: Option<String>,
+    #[serde(default)]
+    pub simplified_paths: Vec<(String, String)>,
+    #[serde(default)]
+    pub contexts: Vec<Context>,
+
+    pub plan_width: u64,
+    pub plan_rows: u64,
+    pub startup_cost: f64,
+    pub total_cost: f64,
+    pub actual_startup_time: Option<f64>,
+    pub actual_total_time: Option<f64>,
+    pub actual_rows: Option<f64>,
+    pub actual_loops: Option<f64>,
+}
+
+impl fmt::Display for PropValue {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use PropValue::*;
+
+        match self {
+            Kbytes(val) => {
+                // TODO(tailhook) use mega/gigabytes
+                val.fmt(f)?;
+                write!(f, "kB")?;
+            }
+            Millis(val) => {
+                // TODO(tailhook) use seconds, minutes, hours?
+                val.fmt(f)?;
+                write!(f, "ms")?;
+            }
+            Expr(val) => {
+                write!(f, "{:?}", val)?;
+            }
+            Index(val) => {
+                val.fmt(f)?;
+            }
+            Relation(val) => {
+                val.fmt(f)?;
+            }
+            Text(val) => {
+                val.fmt(f)?;
+            }
+            Int(val) => {
+                val.fmt(f)?;
+            }
+            Float(val) => {
+                // TODO(tailhook) figure out the best format
+                val.fmt(f)?;
+            }
+
+            ListKbytes(_) => todo!(),
+            ListMillis(_) => todo!(),
+            ListExpr(_) => todo!(),
+            ListIndex(_) => todo!(),
+            ListRelation(_) => todo!(),
+            ListText(_) => todo!(),
+            ListInt(_) => todo!(),
+            ListFloat(_) => todo!(),
+        }
+        Ok(())
+    }
+}
---
+++ src/analyze/tree.rs
@@ -0,0 +1,258 @@
+use std::collections::BTreeMap;
+use std::env;
+use std::fmt;
+
+use crate::analyze::model::{Explain, Plan, IndexCell};
+use crate::analyze::model::{Shape, ChildName, DebugNode};
+
+static NUMBERS: [char; 10] = ['➊', '➋', '➌', '➍', '➎', '➏', '➐', '➑', '➒', '➓'];
+
+
+struct Opt<'a, T>(&'a Option<T>);
+
+pub fn print_debug_plan(explain: &Explain) {
+    if let Some(debug) = &explain.debug_info {
+        if let Some(node) = &debug.full_plan {
+            println!("Debug Plan");
+            print_debug_node("", node, true);
+        }
+    }
+}
+
+fn print_debug_node(prefix: &str, node: &DebugNode, last: bool) {
+    let marker = if last { "└──" } else { "├──" };
+    println!("{prefix}{marker}{shape}{ctx} {node_type}:{parent} {alias}/{rel_name} [{simplified_path}] / {index} \
+             (cost={cost1}..{cost2} actual_time={atime} rows={rows} width={width})",
+             node_type=node.node_type,
+             parent=node.parent_relationship.as_deref().unwrap_or("root"),
+             index=node.index_name.as_deref().unwrap_or("-"),
+             rows=node.plan_rows,
+             width=node.plan_width,
+             cost1=node.startup_cost,
+             cost2=node.total_cost,
+             atime=Opt(&node.actual_total_time),
+             alias=node.alias.as_deref().unwrap_or("-"),
+             rel_name=node.relation_name.as_deref().unwrap_or("-"),
+             simplified_path=node.simplified_paths.iter()
+                 .map(|(alias, attr)| format!("{alias}.{attr}"))
+                 .collect::<Vec<_>>().join(","),
+             shape=node.is_shape.then(|| "+").unwrap_or(""),
+             ctx=node.contexts.iter()
+                 .flat_map(|c| c.index_cell.load())
+                 .next().map(|idx| NUMBERS[idx as usize]) // TODO 10 > nums
+                 .unwrap_or(if node.contexts.is_empty() {' '} else {'*'}),
+    );
+    let prefix = prefix.to_string() + if last { "    " } else { "│   " };
+    let last_idx = node.plans.len().saturating_sub(1);
+    for (idx, node) in node.plans.iter().enumerate() {
+        print_debug_node(&prefix, node, idx == last_idx);
+    }
+}
+
+pub fn print_shape(explain: &Explain) {
+    println!("Shape");
+    if let Some(shape) = &explain.coarse_grained {
+        print_subshape("", None, shape, true);
+    }
+}
+
+fn print_subshape(prefix: &str, attribute: Option<&str>, node: &Shape, last: bool)
+{
+    let marker = if last { "╰──" } else { "├──" };
+    let mut title = Vec::with_capacity(4);
+    if let Some(attribute) = attribute {
+        title.push(".");
+        title.push(attribute);
+    }
+    for (idx, relation) in node.relations.iter().enumerate() {
+        if idx == 0 && !title.is_empty() {
+            title.push(": ");
+        } else if idx > 0 {
+            title.push(", ");
+        }
+        title.push(relation);
+    }
+    let cost = format!("(cost={cost})",
+         cost=node.cost.total_cost,
+    );
+    println!("{prefix}{marker}{ctx} {title} {cost}",
+             title=title.join(""),
+             ctx=node.contexts.iter()
+                 .flat_map(|c| c.index_cell.load())
+                 .next().map(|idx| NUMBERS[idx as usize]) // TODO 10 > nums
+                 .unwrap_or(if node.contexts.is_empty() {' '} else {'*'}),
+    );
+
+    let prefix = prefix.to_string() + if last { "   " } else { "│  " };
+
+    let last_idx = node.children.len().saturating_sub(1);
+    for (idx, ch) in node.children.iter().enumerate() {
+        match &ch.name {
+            ChildName::Pointer { name } => {
+                print_subshape(&prefix, Some(name), &ch.node, idx == last_idx);
+            }
+            _ => {
+                print_subshape(&prefix, None, &ch.node, idx == last_idx);
+            }
+        }
+    }
+}
+
+fn find_indexes(plan: &Plan,
+                buf: &mut BTreeMap<(usize, usize), Vec<IndexCell>>) {
+    if let Some(ctx) = plan.contexts.last() {
+        if ctx.buffer_idx == 0 {
+            buf.entry((ctx.start, ctx.end))
+                .or_insert_with(Vec::new)
+                .push(ctx.index_cell.clone());
+        }
+    }
+    for sub_plan in &plan.subplans {
+        find_indexes(sub_plan, buf);
+    }
+}
+
+fn find_shape_indexes(shape: &Shape,
+                      buf: &mut BTreeMap<(usize, usize), Vec<IndexCell>>) {
+    if let Some(ctx) = shape.contexts.last() {
+        if ctx.buffer_idx == 0 {
+            buf.entry((ctx.start, ctx.end))
+                .or_insert_with(Vec::new)
+                .push(ctx.index_cell.clone());
+        }
+    }
+    for sub in &shape.children {
+        find_shape_indexes(&sub.node, buf);
+    }
+}
+
+fn find_debug_indexes(plan: &DebugNode,
+                      buf: &mut BTreeMap<(usize, usize), Vec<IndexCell>>) {
+    if let Some(ctx) = plan.contexts.last() {
+        if ctx.buffer_idx == 0 {
+            buf.entry((ctx.start, ctx.end))
+                .or_insert_with(Vec::new)
+                .push(ctx.index_cell.clone());
+        }
+    }
+    for sub_plan in &plan.plans {
+        find_debug_indexes(sub_plan, buf);
+    }
+}
+
+pub fn print_contexts(explain: &Explain) {
+    println!("Contexts");
+    let mut context_indexes = BTreeMap::new();
+    if let Some(plan) = &explain.fine_grained {
+        find_indexes(plan, &mut context_indexes);
+    }
+    if let Some(shape) = &explain.coarse_grained {
+        find_shape_indexes(shape, &mut context_indexes);
+    }
+    if env::var_os("_EDGEDB_ANALYZE_DEBUG_PLAN")
+        .map(|x| !x.is_empty()).unwrap_or(false)
+    {
+        if let Some(debug) = &explain.debug_info {
+            if let Some(node) = &debug.full_plan {
+                find_debug_indexes(node, &mut context_indexes);
+            }
+        }
+    }
+
+    let text = &explain.buffers[0];
+    let mut out = String::with_capacity(text.len() + context_indexes.len());
+    let mut offset = 0;
+    let mut iter = context_indexes.iter().enumerate().peekable();
+    while let Some((idx, (&(start, end), cells))) = iter.next() {
+        if start < offset {
+            continue; // TODO(tailhook)
+        }
+        out.push_str(&text[offset..start]);
+        for c in cells {
+            c.store(Some(idx as u32));
+        }
+        if iter.peek().map(|(_, (&(next, _), _))| next > end).unwrap_or(true)
+           && !text[start..end].trim().contains('\n')
+        {
+            out.push_str("\x1B[4m");  // underline
+            out.push(NUMBERS[idx]);  // TODO(tailhook) more than 10
+            out.push(' ');  // some terminals are bad with wide chars
+            out.push_str(&text[start..end]);
+            out.push_str("\x1B[0m");  // reset
+            offset = end;
+        } else {
+            out.push(NUMBERS[idx]);  // TODO(tailhook) more than 10
+            out.push(' ');  // some terminals are bad with wide chars
+            offset = start;
+        }
+    }
+    out.push_str(&text[offset..]);
+    println!("{}", out);
+}
+
+pub fn print_tree(explain: &Explain) {
+    println!("Full Plan Tree");
+    if let Some(tree) = &explain.fine_grained {
+        print_tree_node("", tree, true);
+    }
+}
+
+fn print_tree_node(prefix: &str, node: &Plan, last: bool) {
+    let children = !node.subplans.is_empty();
+    let pipe = node.pipeline.len() > 1;
+    let (m1, m2, m3, m4) = match (last, children, pipe) {
+        (true, true, false) =>   ("╰┬───", "     ", "     ", " │   "), //
+        (false, true, false) =>  ("├┬───", "     ", "     ", "││   "), //
+        (true, false, false) =>  ("╰────", "     ", "     ", "     "), //
+        (false, false, false) => ("├────", "     ", "     ", "│    "),
+
+        (true, true, true) =>    ("╰┬┄┄┄", " ┊   ", " ├───", " │   "), //
+        (false, true, true) =>   ("├┬┄┄┄", "│┊   ", "│├───", "││   "), //
+        (true, false, true) =>   ("╰┬┄┄┄", " ┊   ", " ╰───", " │   "),
+        (false, false, true) =>  ("├┬┄┄┄", "│┊   ", "│╰───", "│    "), //
+    };
+    let last_idx = node.pipeline.len().saturating_sub(1);
+    for (idx, stage) in node.pipeline.iter().enumerate() {
+        let pipelast = idx == last_idx;
+        println!("{prefix}{marker}{ctx} {type} (cost={cost1}..{cost2} rows={rows} width={width})",
+                 marker=if idx == 0 { m1 } else if pipelast { m3 } else { m2 },
+                 type=stage.plan_type,
+                 rows=stage.cost.plan_rows,
+                 width=stage.cost.plan_width,
+                 cost1=stage.cost.startup_cost,
+                 cost2=stage.cost.total_cost,
+                 ctx=if pipelast {
+                     node.contexts.iter()
+                     .flat_map(|c| c.index_cell.load())
+                     .next().map(|idx| NUMBERS[idx as usize]) // TODO 10 > nums
+                     .unwrap_or(if node.contexts.is_empty() {' '} else {'*'})
+                 } else {
+                     ' '
+                 },
+
+        );
+        for prop in &stage.properties {
+            if prop.important {
+                println!("{prefix}{marker}    {name}={value}",
+                         marker=if pipelast { m4 } else { m2 },
+                         name=prop.title,
+                         value=prop.value,
+                );
+            }
+        }
+    }
+    let prefix = prefix.to_string() + if last { " " } else { "│" };
+    let last_idx = node.subplans.len().saturating_sub(1);
+    for (idx, node) in node.subplans.iter().enumerate() {
+        print_tree_node(&prefix, node, idx == last_idx);
+    }
+}
+
+impl<'a, T: fmt::Display> fmt::Display for Opt<'a, T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.0 {
+            None => write!(f, "∅"),
+            Some(v) => v.fmt(f),
+        }
+    }
+}
