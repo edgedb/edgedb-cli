edgedb/edgedb-cli

commit c22c4fc0ca8727db3fad85adf3cb74b4f9db6382

Make "Connecting to..." line more informative (#1156)
Include the instance name, and, if the instance is a Cloud instance,
don't show the host name as it's not very informative.

Also, in interactive mode, erase the "Connecting to..." line once the
connection has been established for a cleaner initial look.

src/interactive.rs
@@ -149,11 +149,7 @@ pub fn main(options: Options, cfg: Config) -> Result<(), anyhow::Error> {
 pub async fn _main(options: Options, mut state: repl::State, cfg: Config)
     -> anyhow::Result<()>
 {
-    let mut conn = state.conn_params.connect().await?;
-    let fetched_version = conn.get_version().await?;
-    echo!("EdgeDB".light_gray(), fetched_version.to_string().light_gray(),
-        format_args!("(repl {})", env!("CARGO_PKG_VERSION")).fade());
-    state.last_version = Some(fetched_version.clone());
+    state.reconnect().await?;
     if let Some(config_path) = &cfg.file_name {
         echo!(
             format_args!("Applied {} configuration file",
@@ -162,9 +158,6 @@ pub async fn _main(options: Options, mut state: repl::State, cfg: Config)
     }
     echo!(r#"Type \help for help, \quit to quit."#.light_gray());
     state.set_history_limit(state.history_limit).await?;
-    state.connection = Some(conn);
-    state.set_idle_transaction_timeout().await?;
-    state.read_state();
     match _interactive_main(&options, &mut state).await {
         Ok(()) => return Ok(()),
         Err(e) => {

src/repl.rs
@@ -20,7 +20,8 @@ use crate::async_util::timeout;
 use crate::connect::Connection;
 use crate::connect::Connector;
 use crate::portable::ver;
-use crate::print;
+use crate::print::{self, Highlight};
+use crate::echo;
 use crate::prompt::variable::VariableInput;
 use crate::prompt::{self, Control};
 use crate::analyze;
@@ -121,19 +122,8 @@ impl PromptRpc {
 
 impl State {
     pub async fn reconnect(&mut self) -> anyhow::Result<()> {
-        let mut conn = self.conn_params.connect().await?;
-        let fetched_version = conn.get_version().await?;
-        if self.last_version.as_ref() != Some(&fetched_version) {
-            println!("{} {} (repl v{})",
-                "EdgeDB".light_gray(),
-                fetched_version.to_string().light_gray(),
-                env!("CARGO_PKG_VERSION"));
-            self.last_version = Some(fetched_version.to_owned());
-        }
-        conn.set_state(self.edgeql_state.clone());
-        self.database = self.conn_params.get()?.database().into();
-        self.connection = Some(conn);
-        self.set_idle_transaction_timeout().await?;
+        let db = self.conn_params.get()?.database().to_owned();
+        self.try_connect(&db).await?;
         Ok(())
     }
     pub async fn set_idle_transaction_timeout(&mut self) -> anyhow::Result<()> {
@@ -154,23 +144,27 @@ impl State {
         }
         Ok(())
     }
+    fn print_banner(&self, version: &ver::Build) -> anyhow::Result<()> {
+        echo!(
+            format!("{}\rEdgeDB", ansi_escapes::EraseLine.to_string()).light_gray(),
+            version.to_string().light_gray(),
+            format_args!("(repl {})", env!("CARGO_PKG_VERSION")).fade(),
+        );
+        Ok(())
+    }
     pub async fn try_connect(&mut self, database: &str) -> anyhow::Result<()> {
         let mut params = self.conn_params.clone();
         params.database(database)?;
-        let mut conn = params.connect().await?;
+        let mut conn = params.connect_interactive().await?;
         let fetched_version = conn.get_version().await?;
         if self.last_version.as_ref() != Some(&fetched_version) {
-            println!("{} {} (repl v{})",
-                "EdgeDB".light_gray(),
-                fetched_version.to_string().light_gray(),
-                env!("CARGO_PKG_VERSION"));
+            self.print_banner(&fetched_version)?;
             self.last_version = Some(fetched_version.to_owned());
         }
         self.conn_params = params;
         self.database = database.into();
-        self.edgeql_state = conn.get_state().clone();
-        self.edgeql_state_desc = conn.get_state_desc();
         self.connection = Some(conn);
+        self.read_state();
         self.set_idle_transaction_timeout().await?;
         Ok(())
     }

Cargo.toml
@@ -18,6 +18,7 @@ edgedb-derive = {git = "https://github.com/edgedb/edgedb-rust/"}
 edgedb-errors = {git = "https://github.com/edgedb/edgedb-rust/"}
 edgedb-tokio = {git = "https://github.com/edgedb/edgedb-rust/", features=["admin_socket", "unstable"]}
 snafu = {version="0.7.0", features=["backtraces"]}
+ansi-escapes = "0.1"
 anyhow = "1.0.23"
 bytes = "1.0.1"
 blake2b_simd = "1.0.0"

src/connect.rs
@@ -151,20 +151,44 @@ impl Connector {
         self
     }
     pub async fn connect(&self) -> Result<Connection, anyhow::Error> {
+        self._connect(false).await
+    }
+
+    pub async fn connect_interactive(&self) -> Result<Connection, anyhow::Error> {
+        self._connect(true).await
+    }
+
+    async fn _connect(&self, interactive: bool) -> Result<Connection, anyhow::Error> {
         let cfg = self.config.as_ref().map_err(Clone::clone)?;
         let conn = tokio::select!(
             conn = Connection::connect(&cfg) => conn?,
-            _ = self.print_warning(cfg) => unreachable!(),
+            _ = self.print_warning(cfg, interactive) => unreachable!(),
         );
         Ok(conn)
     }
 
-    async fn print_warning(&self, cfg: &Config)
+    fn warning_msg(&self, cfg: &Config) -> String {
+        let desc = match cfg.instance_name() {
+            Some(edgedb_tokio::InstanceName::Cloud { org_slug: org, name }) =>
+                format!("EdgeDB Cloud instance '{}/{}'", org, name),
+            Some(edgedb_tokio::InstanceName::Local(name)) =>
+                format!("EdgeDB instance '{}' at {}", name, cfg.display_addr()),
+            _ =>
+                format!("EdgeDB instance at {}", cfg.display_addr())
+        };
+        format!("Connecting to {}...", desc)
+    }
+
+    async fn print_warning(&self, cfg: &Config, interactive: bool)
         -> Result<Connection, Error>
     {
         sleep(Duration::new(1, 0)).await;
-        eprintln!("Connecting to EdgeDB instance at {}...",
-            cfg.display_addr());
+        let msg = self.warning_msg(cfg);
+        if interactive {
+            eprint!("{}", msg);
+        } else {
+            eprintln!("{}", msg);
+        }
         pending().await
     }
     pub fn get(&self) -> anyhow::Result<&Config, ArcError> {

commit 14bad166b791c80f56d366a0908bf665cc0f8d9e

Restore preservation of session configuration across reconnects
This regressed in #1156

src/interactive.rs
@@ -149,7 +149,7 @@ pub fn main(options: Options, cfg: Config) -> Result<(), anyhow::Error> {
 pub async fn _main(options: Options, mut state: repl::State, cfg: Config)
     -> anyhow::Result<()>
 {
-    state.reconnect().await?;
+    state.connect().await?;
     if let Some(config_path) = &cfg.file_name {
         echo!(
             format_args!("Applied {} configuration file",

src/repl.rs
@@ -121,9 +121,23 @@ impl PromptRpc {
 }
 
 impl State {
-    pub async fn reconnect(&mut self) -> anyhow::Result<()> {
+    pub async fn connect(&mut self) -> anyhow::Result<()> {
         let db = self.conn_params.get()?.database().to_owned();
         self.try_connect(&db).await?;
+        Ok(())
+    }
+    pub async fn reconnect(&mut self) -> anyhow::Result<()> {
+        let db = self.conn_params.get()?.database().to_owned();
+        let cur_state = self.edgeql_state.clone();
+        let cur_state_desc = self.edgeql_state_desc.clone();
+        self.try_connect(&db).await?;
+        if let Some(conn) = &mut self.connection {
+            if cur_state_desc == self.edgeql_state_desc {
+                conn.set_state(cur_state);
+            } else {
+                eprintln!("Discarding session configuration because server configuration layout has changed.");
+            }
+        }
         Ok(())
     }
     pub async fn set_idle_transaction_timeout(&mut self) -> anyhow::Result<()> {