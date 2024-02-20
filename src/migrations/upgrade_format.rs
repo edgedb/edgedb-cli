595d74f205e58c7d928dbf2d5d52eee30d19350e
Quin Lynch <lynchquin@gmail.com>
2024-02-20 19:55:52 -0400

    add test

--- src/migrations/upgrade_format.rs
+++ src/migrations/upgrade_format.rs
@@ -15,7 +15,11 @@ pub async fn upgrade_format(
 ) -> anyhow::Result<()> {
     let ctx = Context::from_project_or_config(&params.cfg, false).await?;
 
-    let names = read_names(&ctx).await?;
+    _upgrade_format(&ctx).await
+}
+
+async fn _upgrade_format(context: &Context) -> anyhow::Result<()> {
+    let names = read_names(context).await?;
 
     let old_filename = Regex::new(r"^\d{5}$")?;
     let new_filename = Regex::new(r"^\d{5}-[a-z0-9]{7}$")?;
@@ -46,4 +50,44 @@ async fn upgrade_format_of_file(file: &PathBuf, num: u64) -> anyhow::Result<()> 
     fs::rename(file, new_name)?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use std::fs;
+    use regex::Regex;
+    use crate::migrations::Context;
+    use crate::migrations::migration::{read_file, read_names};
+    use crate::migrations::upgrade_format::_upgrade_format;
+
+    #[tokio::test]
+    async fn test_upgrade() {
+        use std::env;
+
+        let mut schema_dir = env::current_dir().unwrap();
+        schema_dir.push("tests/migrations/db3");
+
+        let ctx = Context{schema_dir, edgedb_version: None, quiet: false};
+
+        _upgrade_format(&ctx).await.unwrap();
+
+        let files = read_names(&ctx).await.unwrap();
+        let re = Regex::new(r"^(\d{5})-([a-z0-9]{7})$").unwrap();
+
+        for file in files {
+            let migration = read_file(&file, true).await.unwrap();
+            let name = file.file_stem().unwrap().to_str().unwrap();
+
+            // verify that the filename matches the new format
+            let re_match = re.captures(name).unwrap();
+
+            // verify the migration ID in the filename starts with the ID in the migration
+            let migration_part = re_match.get(2).unwrap().as_str();
+            assert!(migration.id.starts_with(migration_part));
+
+            // rename the file back to the old one
+            let target = file.parent().unwrap().join(format!("{:05}.edgeql", re_match.get(1).unwrap().as_str()));
+            fs::rename(file, target).unwrap();
+        }
+    }
 }
