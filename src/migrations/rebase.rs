79dcb35ff7aaa4029fd2e90eb3e753e209aecfd2
Quin Lynch <quin@edgedb.com>
2024-03-07 13:09:00 -0400

    clippy

--- src/migrations/rebase.rs
+++ src/migrations/rebase.rs
@@ -1,20 +1,20 @@
 use std::collections::HashMap;
-use std::iter::FromIterator;
+
 use fs_err as fs;
 
-use std::ops::Index;
+
 use std::path::PathBuf;
-use anyhow::{anyhow, Context as _};
+use anyhow::{Context as _};
 use colorful::Colorful;
 use indexmap::IndexMap;
 use crate::connect::Connection;
-use crate::migrations::{Context, create, db_migration, migrate, migration};
+use crate::migrations::{Context, create, migrate, migration};
 use crate::migrations::create::{MigrationKey, MigrationToText};
 use crate::migrations::db_migration::{DBMigration, read_all};
-use crate::migrations::dev_mode::rebase_to_schema;
-use crate::migrations::extract::DatabaseMigration;
-use crate::migrations::grammar::parse_migration;
-use crate::migrations::migrate::apply_migrations_inner;
+
+
+
+
 use crate::migrations::migration::MigrationFile;
 use crate::print;
 
