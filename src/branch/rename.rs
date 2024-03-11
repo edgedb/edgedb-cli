252fec23931012f64f6c7b085b79ba399b84e807
Quin Lynch <lynchquin@gmail.com>
2024-03-11 12:49:59 -0300

    allow rename of current branch

--- src/branch/rename.rs
+++ src/branch/rename.rs
@@ -1,14 +1,19 @@
+use crate::branch::connections::get_connection_to_modify;
 use crate::branch::context::Context;
 use crate::branch::option::Rename;
+use crate::options::Options;
 use crate::connect::Connection;
 use crate::print;
 
 pub async fn main(
     options: &Rename,
-    _context: &Context,
+    context: &Context,
     connection: &mut Connection,
+    cli_opts: &Options,
 ) -> anyhow::Result<()> {
-    let status = connection
+    let mut modify_connection = get_connection_to_modify(&context.branch, &cli_opts, connection).await?;
+
+    let status = modify_connection.connection
         .execute(
             &format!(
                 "alter branch {0}{2} rename to {1}",
@@ -23,5 +28,12 @@ pub async fn main(
     print::completion(status);
 
     eprintln!("Renamed branch {} to {}", options.old_name, options.new_name);
+
+    if options.old_name == context.branch {
+        context.update_branch(&options.new_name).await?;
+    }
+
+    modify_connection.clean().await?;
+
     Ok(())
 }
