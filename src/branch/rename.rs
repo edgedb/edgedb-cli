2d08d26e9ec96a1349d871b12333f028288b33ba
Quin Lynch <49576606+quinchs@users.noreply.github.com>
2024-03-11 16:12:05 -0300

    Branches: Allow renaming of current branch (#1245)
    * allow rename of current branch
    
    * pass options to rename command
    
    * don't create new connection unless needed
    
    * pass options to rename

--- src/branch/rename.rs
+++ src/branch/rename.rs
@@ -1,13 +1,31 @@
+use crate::branch::connections::get_connection_to_modify;
 use crate::branch::context::Context;
 use crate::branch::option::Rename;
+use crate::options::Options;
 use crate::connect::Connection;
 use crate::print;
 
 pub async fn main(
     options: &Rename,
-    _context: &Context,
+    context: &Context,
     connection: &mut Connection,
+    cli_opts: &Options,
 ) -> anyhow::Result<()> {
+    if options.old_name == context.branch {
+        let mut modify_connection = get_connection_to_modify(&context.branch, &cli_opts, connection).await?;
+        rename(&mut modify_connection.connection, &options).await?;
+        modify_connection.clean().await?;
+        context.update_branch(&options.new_name).await?;
+    } else {
+        rename(connection, &options).await?;
+    }
+
+    eprintln!("Renamed branch {} to {}", options.old_name, options.new_name);
+
+    Ok(())
+}
+
+async fn rename(connection: &mut Connection, options: &Rename) -> anyhow::Result<()> {
     let status = connection
         .execute(
             &format!(
@@ -22,6 +40,5 @@ pub async fn main(
 
     print::completion(status);
 
-    eprintln!("Renamed branch {} to {}", options.old_name, options.new_name);
     Ok(())
 }
