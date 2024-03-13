c8c4487d27efc95adcd5d72e4c73357a847335f9
Quin Lynch <lynchquin@gmail.com>
2024-03-13 15:34:14 -0300

    wipe the provided branch, not the connected one

--- src/branch/wipe.rs
+++ src/branch/wipe.rs
@@ -1,15 +1,24 @@
 use crate::branch::context::Context;
 use crate::branch::option::Wipe;
 use crate::commands::ExitCode;
-use crate::connect::Connection;
+use crate::connect::{Connection, Connector};
 use crate::portable::exit_codes;
 use crate::{print, question};
+use crate::branch::connections::connect_if_branch_exists;
 
 pub async fn main(
     options: &Wipe,
     _context: &Context,
-    connection: &mut Connection,
+    connector: &mut Connector,
 ) -> anyhow::Result<()> {
+    let connection = connect_if_branch_exists(connector.database(&options.branch)?).await?;
+
+    if connection.is_none() {
+        anyhow::bail!("Branch '{}' doesn't exist", &options.branch)
+    }
+
+    let mut connection = connection.unwrap();
+
     if !options.non_interactive {
         let q = question::Confirm::new_dangerous(format!(
             "Do you really want to wipe \
@@ -17,7 +26,7 @@ pub async fn main(
             options.branch
         ));
         if !connection.ping_while(q.async_ask()).await? {
-            print::error("Canceled.");
+            print::error("Canceled by user.");
             return Err(ExitCode::new(exit_codes::NOT_CONFIRMED).into());
         }
     }
