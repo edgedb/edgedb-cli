e427f8117d608cce788f8596bf48779520c24e40
Aljaž Mur Eržen <aljaz@edgedb.com>
2024-10-09 11:10:24 +0200

    print at interactive

--- src/interactive.rs
+++ src/interactive.rs
@@ -343,7 +343,10 @@ async fn execute_query(
 
     if !items.can_contain_data() {
         match items.complete().await {
-            Ok(res) => print::completion(&res.status_data),
+            Ok(res) => {
+                print::warnings(&res.warnings, statement)?;
+                print::completion(&res.status_data)
+            }
             Err(e) if e.is::<StateMismatchError>() => {
                 return Err(RetryStateError)?;
             }
@@ -503,7 +506,10 @@ async fn execute_query(
             }
         }
     }
-    items.complete().await?;
+
+    let res = items.complete().await?;
+    print::warnings(&res.warnings, statement)?;
+
     if state.print_stats != Off {
         eprintln!(
             "{}",
