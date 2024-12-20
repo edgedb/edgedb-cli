e9f22536c14fb3e043d86a4694283727845693f6
Aljaž Mur Eržen <aljaz@edgedb.com>
2024-12-20 19:31:02 +0100

    fix

--- tests/portable_smoke.rs
+++ tests/portable_smoke.rs
@@ -272,6 +272,14 @@ fn install() {
         .success();
 
     Command::new("edgedb")
+        .arg("--instance=second")
+        .arg("extension")
+        .arg("list")
+        .assert()
+        .context("extension-list", "basic list of the installed extensions")
+        .success();
+
+    Command::new("edgedb")
         .arg("instance")
         .arg("destroy")
         .arg("second")
@@ -305,14 +313,5 @@ fn install() {
         .arg("SELECT 1")
         .assert()
         .context("query-1a", "late query of `inst1`")
-        .success();
-
-    Command::new("edgedb")
-        .arg("--instance")
-        .arg("inst1")
-        .arg("extension")
-        .arg("list")
-        .assert()
-        .context("extension-list", "basic list of the installed extensions")
         .success();
 }
