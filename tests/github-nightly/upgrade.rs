246c0b9b1bcf3de83ffb9c4ac8cd406c0d0a0fde
Aljaž Mur Eržen <aljaz@edgedb.com>
2025-01-21 18:12:25 +0100

    update "upgrade" test

--- tests/github-nightly/upgrade.rs
+++ tests/github-nightly/upgrade.rs
@@ -19,45 +19,42 @@ fn package(tagname: &str, dockerfile: &str) -> anyhow::Result<()> {
     run_systemd(
         tagname,
         r###"
-        edgedb server install --version=1-rc.5
-        edgedb instance create test1 --version=1-rc.5
+        edgedb server install --version=4.8
+        edgedb instance create test1 --version=4.8
 
-        ver1=$(edgedb -Itest1 --wait-until-available=60s --tab-separated query '
-            SELECT sys::get_version_as_str()
-        ')
-        [[ $ver1 =~ ^1\.0-rc\.5\+ ]]
+        ver1=$(edgedb -Itest1 --wait-until-available=60s query --output-format=tab-separated \
+            'SELECT sys::get_version_as_str()')
+        [[ $ver1 =~ ^4\.8 ]]
 
         edgedb --wait-until-available=60s -Itest1 query '
             CREATE TYPE Type1 {
                 CREATE PROPERTY prop1 -> str;
             }
         ' 'INSERT Type1 { prop1 := "value1" }'
-        if ! edgedb instance upgrade -I test1 --to-version=1; then
+        if ! edgedb instance upgrade -I test1 --to-version=5 --non-interactive; then
             res=$?
             journalctl -xe
             exit $res
         fi
-        ver2=$(edgedb -Itest1 --wait-until-available=60s --tab-separated query '
-            SELECT sys::get_version_as_str()
-        ')
-        [[ $ver2 =~ ^1\.[0-9]+\+ ]]
+        ver2=$(edgedb -Itest1 --wait-until-available=60s query --output-format=tab-separated \
+            'SELECT sys::get_version_as_str()')
+        [[ $ver2 =~ ^5\.[0-9]+\+ ]]
 
-        val=$(edgedb -Itest1 --wait-until-available=60s --tab-separated \
-              query 'SELECT Type1 { prop1 }')
+        val=$(edgedb -Itest1 --wait-until-available=60s query --output-format=tab-separated \
+            'SELECT Type1 { prop1 }')
         test "$val" = "value1"
 
-        if ! edgedb server revert test1 --no-confirm; then
+        if ! edgedb instance revert -I test1 --no-confirm; then
             res=$?
             journalctl -xe
             exit $res
         fi
-        ver2=$(edgedb -Itest1 --wait-until-available=60s --tab-separated query '
-            SELECT sys::get_version_as_str()
-        ')
-        [[ $ver1 =~ ^1\.0-rc\.5\+ ]]
+        ver2=$(edgedb -Itest1 --wait-until-available=60s query --output-format=tab-separated \
+            'SELECT sys::get_version_as_str()')
+        [[ $ver1 =~ ^4\.8 ]]
 
-        val=$(edgedb -Itest1 --wait-until-available=60s --tab-separated \
-              query 'SELECT Type1 { prop1 }')
+        val=$(edgedb -Itest1 --wait-until-available=60s query --output-format=tab-separated \
+            'SELECT Type1 { prop1 }')
         test "$val" = "value1"
     "###,
     )
