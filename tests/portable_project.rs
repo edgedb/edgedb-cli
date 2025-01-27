92426263ecac73119ed6fb3745caecde7f8f556c
Aljaž Mur Eržen <aljaz@edgedb.com>
2025-01-27 19:16:52 +0100

    add tests

--- tests/portable_project.rs
+++ tests/portable_project.rs
@@ -237,3 +237,118 @@ fn project_link_and_init() {
         .context("destroy-2", "should unlink and destroy project")
         .success();
 }
+
+#[test]
+fn hooks() {
+    Command::new("edgedb")
+        .arg("--version")
+        .assert()
+        .context("version", "command-line version option")
+        .success()
+        .stdout(predicates::str::contains(EXPECTED_VERSION));
+
+    Command::new("edgedb")
+        .arg("instance")
+        .arg("create")
+        .arg("inst2")
+        .arg("master")
+        .arg("--non-interactive")
+        .assert()
+        .context("instance-create", "")
+        .success();
+
+    Command::new("edgedb")
+        .current_dir("tests/proj/project3")
+        .arg("project")
+        .arg("init")
+        .arg("--link")
+        .arg("--server-instance=inst2")
+        .arg("--non-interactive")
+        .assert()
+        .context("project-init", "")
+        .success()
+        .stderr(ContainsHooks {
+            expected: &[
+                "project.init.after",
+                "migration.apply.before",
+                "migration.apply.after",
+            ],
+        });
+
+    Command::new("edgedb")
+        .current_dir("tests/proj/project3")
+        .arg("branch")
+        .arg("switch")
+        .arg("--create")
+        .arg("--empty")
+        .arg("another")
+        .assert()
+        .context("branch-switch", "")
+        .success()
+        .stderr(ContainsHooks {
+            expected: &["branch.switch.before", "branch.switch.after"],
+        });
+
+    Command::new("edgedb")
+        .current_dir("tests/proj/project3")
+        .arg("branch")
+        .arg("merge")
+        .arg("master")
+        .assert()
+        .context("branch-merge", "")
+        .success()
+        .stderr(ContainsHooks {
+            expected: &["migration.apply.before", "migration.apply.after"],
+        });
+
+    Command::new("edgedb")
+        .current_dir("tests/proj/project3")
+        .arg("branch")
+        .arg("wipe")
+        .arg("another")
+        .arg("--non-interactive")
+        .assert()
+        .context("branch-wipe", "")
+        .success()
+        .stderr(ContainsHooks {
+            expected: &["branch.wipe.before", "branch.wipe.after"],
+        });
+}
+
+#[derive(Debug)]
+struct ContainsHooks<'a> {
+    expected: &'a [&'static str],
+}
+
+impl<'a> predicates::Predicate<str> for ContainsHooks<'a> {
+    fn eval(&self, variable: &str) -> bool {
+        let re = regex::RegexBuilder::new(r"^hook ([a-z.]+):")
+            .multi_line(true)
+            .build()
+            .unwrap();
+        let found_hooks: Vec<_> = re
+            .captures_iter(variable)
+            .map(|c| c.extract::<1>().1[0])
+            .collect();
+
+        self.expected == found_hooks.as_slice()
+    }
+}
+
+impl<'a> predicates::reflection::PredicateReflection for ContainsHooks<'a> {
+    fn parameters<'b>(
+        &'b self,
+    ) -> Box<dyn Iterator<Item = predicates::reflection::Parameter<'b>> + 'b> {
+        let mut params = std::vec![];
+        for e in self.expected {
+            params.push(predicates::reflection::Parameter::new("hook", e));
+        }
+        Box::new(params.into_iter())
+    }
+}
+
+impl<'a> std::fmt::Display for ContainsHooks<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Debug::fmt(&self, f)
+    }
+}
